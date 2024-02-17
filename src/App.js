ae8a2352cf04c4b3298eea84695be42793a28ffd
Ravi Anand <ravianand291@gmail.com>
2024-02-18 01:57:38 +0530

    onecall and date structure

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Inputs from "./Components/Inputs";
 import TemperatureAndDetails from "./Components/TemperatureAndDetails";
 import TimeAndLocation from "./Components/TimeAndLocation";
 import TopButtons from "./Components/TopButtons";
+import getFormattedWeatherData from "./utils/weatherDetails";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  // fetchWeather();
+
   return (
     <div className="App">
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
---
+++ src/utils/weatherDetails.js
@@ -0,0 +1,70 @@
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData = (type, params) => {
+  const url = new URL(BASE_URL + "/" + type);
+  url.search = new URLSearchParams({
+    ...params,
+    appid: process.env.REACT_APP_API_KEY,
+  });
+
+  console.log(process.env);
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6);
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
